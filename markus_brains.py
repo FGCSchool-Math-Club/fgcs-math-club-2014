be909d93729090b1026de8943875c7839c127d29
Markus Roberts <Markus@reality.com>
2014-11-08 22:51:41 -0800

    Racing brains

--- markus_brains.py
+++ markus_brains.py
@@ -52,3 +52,23 @@ class RunnerBrain(CritterBrain):
             return "Accelerate 2.0"
 
 Brains.register(RunnerBrain)
+
+class RacerBrain(CritterBrain):
+    code = "R"
+    max_speed = None
+    max_acceleration = None
+    def on_collision(self,dir,other,senses):
+        pass
+    def on_attack(self,dir,attacker,senses):
+        pass
+    def on_tick(self,senses):
+        if self.max_speed is None:
+            self.max_speed = uniform(0.8,2.0)
+            self.max_acceleration = uniform(1.01,max([1.1,self.max_speed]))
+            #print((self.max_speed,self.max_acceleration))
+        acceleration = self.max_speed/senses['body'].speed
+        if acceleration > self.max_acceleration:
+            acceleration = self.max_acceleration
+        return "Accelerate {}".format(acceleration)
+
+Brains.register(RacerBrain)
