c3f94202338902ff4cd9d922c04df89506132898
UleePaz <jenhardacker@yahoo.com>
2014-11-20 15:53:37 -0800

    Update radon_brains.py

--- radon_brains.py
+++ radon_brains.py
@@ -88,7 +88,7 @@ Brains.register(TastingBrain)
 class RacerBrain(CritterBrain):
     code = "R"
     max_speed = 1.24
-    max_acceleration = 1.24
+    max_acceleration = 1.30
     def on_collision(self,dir,other,senses):
         pass
     def on_attack(self,dir,attacker,senses):
