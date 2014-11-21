c19cedad414937860daed985d4cd531e59757852
StickGuyNinja <StickGuyNinja@users.noreply.github.com>
2014-11-20 16:30:58 -0800

    Update xenon_brains.py

--- xenon_brains.py
+++ xenon_brains.py
@@ -86,8 +86,8 @@ Brains.register(TastingBrain)
 
 class RacerBrain(CritterBrain):
     code = "R"
-    max_speed = 1.3213956
-    max_acceleration = 1.017028
+    max_speed = 1.32139572
+    max_acceleration = 1.0170291
     def on_collision(self,dir,other,senses):
         pass
     def on_attack(self,dir,attacker,senses):
