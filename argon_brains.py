09901401e7984a49fa4493995a736de56d09fe2f
username497 <MaxwellJR@users.noreply.github.com>
2014-11-20 16:28:24 -0800

    Update argon_brains.py

--- argon_brains.py
+++ argon_brains.py
@@ -87,7 +87,7 @@ Brains.register(TastingBrain)
 
 class RacerBrain(CritterBrain):
     code = "R"
-    max_speed = 1.3214
+    max_speed = 1.3215
     max_acceleration = 1.01701
     def on_collision(self,dir,other,senses):
         pass
