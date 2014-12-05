81c77eba673bfda48f8dc03b0ec37143e3e787ee
UleePaz <jenhardacker@yahoo.com>
2014-12-04 16:21:00 -0800

    Update radon_brains.py

--- radon_brains.py
+++ radon_brains.py
@@ -20,7 +20,7 @@ class LookingBrain(CritterBrain):
     def on_attack(self,dir,attacker,senses):
         pass
     def on_tick(self,senses):
-        yums = [s for s in senses['hearing'] if s.text == "scrwo you"]
+        yums = [s for s in senses['hearing'] if s.text == "Yum"]
         if yums:
             self.time_since_yum = 0
         else:
